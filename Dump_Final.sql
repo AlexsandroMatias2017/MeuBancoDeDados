d9d49f0bee997c9a7ee8e26893ebfa00392c967f
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-06-09 12:58:46 -0300

    Adicionada a chave estrangeira na tabela nota

--- Dump_Final.sql
+++ Dump_Final.sql
@@ -52,7 +52,7 @@ CREATE TABLE `curso` (
   `idcurso` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`idcurso`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +61,7 @@ CREATE TABLE `curso` (
 
 LOCK TABLES `curso` WRITE;
 /*!40000 ALTER TABLE `curso` DISABLE KEYS */;
-INSERT INTO `curso` VALUES (2,'Ciência de Dados'),(15,'Tecnólogo em Redes'),(16,'Tecnólogo em Análise em Desenvolvimento de Sistemas'),(17,'Engenharia da Computação'),(18,'Licenciatura em Informática'),(19,'Licenciatura em Matemática'),(20,'Ciência de Dados'),(21,'Tecnólogo em Redes'),(22,'Tecnólogo em Análise em Desenvolvimento de Sistemas'),(23,'Engenharia da Computação'),(24,'Licenciatura em Informática'),(25,'Licenciatura em Matemática'),(26,'Ciência de Dados'),(27,'Engenharia da Automação');
+INSERT INTO `curso` VALUES (2,'Ciência de Dados'),(15,'Tecnólogo em Redes'),(16,'Tecnólogo em Análise em Desenvolvimento de Sistemas'),(17,'Engenharia da Computação'),(18,'Licenciatura em Informática'),(19,'Licenciatura em Matemática'),(20,'Ciência de Dados'),(21,'Tecnólogo em Redes'),(22,'Tecnólogo em Análise em Desenvolvimento de Sistemas'),(23,'Engenharia da Computação'),(24,'Licenciatura em Informática'),(25,'Licenciatura em Matemática'),(26,'Ciência de Dados'),(27,'Engenharia da Automação'),(28,'Bacharelado em Matemática');
 /*!40000 ALTER TABLE `curso` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,7 +78,7 @@ CREATE TABLE `disciplina` (
   `cargahoraria` int(11) DEFAULT NULL,
   PRIMARY KEY (`iddisciplina`),
   UNIQUE KEY `nome_UNIQUE` (`nome`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,7 +87,7 @@ CREATE TABLE `disciplina` (
 
 LOCK TABLES `disciplina` WRITE;
 /*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
-INSERT INTO `disciplina` VALUES (1,'Estrutura de Dados',65),(2,'Banco de Dados 1',60),(3,'Banco de Dados 2',50),(4,'Arquitetura de Computadores',40),(5,'Sistema Operacionais',45),(6,'Português Instrumental',30),(7,'Inglês Instrumental',25),(8,'Programação Orientada a Objetos',90),(9,'Desenvolvimento Web 1',70),(10,'Desenvolvimento Web 2',70),(11,'Cálculo 1',120),(12,'Cálculo 2',100),(13,'Cálculo 3',85),(14,'Redes 1',65),(15,'Redes 2',55),(16,'Sistemas Embarcados',45),(17,'LIBRAS',30),(18,'Introdução à Programação',120),(19,'Ética Profissional',25),(20,'Cálculo Numérico',60);
+INSERT INTO `disciplina` VALUES (1,'Estrutura de Dados',65),(2,'Banco de Dados 1',60),(3,'Banco de Dados 2',50),(4,'Arquitetura de Computadores',40),(5,'Sistema Operacionais',45),(6,'Português Instrumental',30),(7,'Inglês Instrumental',25),(8,'Programação Orientada a Objetos',90),(9,'Desenvolvimento Web 1',70),(10,'Desenvolvimento Web 2',70),(11,'Cálculo 1',120),(12,'Cálculo 2',100),(13,'Cálculo 3',85),(14,'Redes 1',65),(15,'Redes 2',55),(16,'Sistemas Embarcados',45),(17,'LIBRAS',30),(18,'Introdução à Programação',120),(19,'Ética Profissional',25),(20,'Cálculo Numérico',60),(21,'Geometria Analítica',NULL);
 /*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,9 +102,12 @@ CREATE TABLE `nota` (
   `nota1` float(2,1) NOT NULL,
   `nota2` float(2,1) NOT NULL,
   `idnota` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `fkaluno` int(11) DEFAULT NULL,
-  PRIMARY KEY (`idnota`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `fkdisciplina` int(10) NOT NULL,
+  `fkaluno` int(10) NOT NULL,
+  PRIMARY KEY (`idnota`),
+  KEY `fk_nota_1_idx` (`fkdisciplina`),
+  KEY `fk_nota_2_idx` (`fkaluno`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -113,6 +116,7 @@ CREATE TABLE `nota` (
 
 LOCK TABLES `nota` WRITE;
 /*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+INSERT INTO `nota` VALUES (9.0,8.0,2,4,4),(8.5,6.0,3,1,1),(3.0,7.0,4,3,1),(5.0,5.5,5,3,5),(6.0,9.9,6,3,6),(6.0,4.9,7,4,10),(1.0,6.0,8,4,12),(9.9,7.0,9,5,20),(7.0,8.0,10,9,17),(8.0,9.0,11,7,7),(9.0,1.0,12,12,7),(5.0,2.0,13,11,8),(6.0,7.0,14,10,10),(4.0,8.5,15,10,11),(8.0,7.8,16,10,8),(2.8,7.7,17,10,4),(4.9,8.8,18,8,3),(5.5,9.9,19,9,13),(7.0,7.0,20,1,7),(6.0,7.0,22,15,10);
 /*!40000 ALTER TABLE `nota` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +131,7 @@ CREATE TABLE `professor` (
   `idprofessor` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`idprofessor`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +140,7 @@ CREATE TABLE `professor` (
 
 LOCK TABLES `professor` WRITE;
 /*!40000 ALTER TABLE `professor` DISABLE KEYS */;
-INSERT INTO `professor` VALUES (1,'Anderson Moreira'),(2,'Paulo Abadie'),(3,'Marcos André'),(4,'Renata Dantas'),(5,'Aida Ferreira'),(6,'Renata Freire'),(7,'Rafael Roque'),(8,'Marco Domingues'),(9,'Paulo Gonçalves'),(10,'Fernanda Celci');
+INSERT INTO `professor` VALUES (1,'Anderson Moreira'),(2,'Paulo Abadie'),(3,'Marcos André'),(4,'Renata Dantas'),(5,'Aida Ferreira'),(6,'Renata Freire'),(7,'Rafael Roque'),(8,'Marco Domingues'),(9,'Paulo Gonçalves'),(10,'Fernanda Celci'),(11,'Uribatan Ramos');
 /*!40000 ALTER TABLE `professor` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -149,4 +153,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-07 23:54:30
+-- Dump completed on 2019-06-09 12:55:05
