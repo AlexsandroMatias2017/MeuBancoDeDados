af20f340bc797be378f10c5afe21d713dc9ea343
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-08-05 12:03:56 -0300

    Iniciadas a modelagem das classes em Java

---
+++ qacademicoJava/src/qacademico/entidades/Aluno.java
@@ -0,0 +1,9 @@
+package qacademico.entidades;
+
+public class Aluno
+{
+	int idAluno;
+	private String nome;
+	private String sobrenome;
+	private String telefone;
+}
---
+++ qacademicoJava/src/qacademico/entidades/Nota.java
@@ -0,0 +1,4 @@
+package qacademico.entidades;
+
+public class Nota
+{}
