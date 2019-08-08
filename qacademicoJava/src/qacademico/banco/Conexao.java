ad2d96a0ef7ecf28cf3b6baf7dc683af3c96b96f
pb7p <pb7p@MI00273033.petrobras.biz>
2019-08-08 12:46:04 -0300

    Inicializada a preparação das classes de conexão com o banco e suas exceções.

---
+++ qacademicoJava/src/qacademico/banco/Conexao.java
@@ -0,0 +1,16 @@
+package qacademico.banco;
+
+//
+// Visão geral do JDBC
+// JDBC (Java Database Connectivity): API padr�o do Java para acesso a dados
+// P�ginas oficiais:
+// https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/
+// https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html
+// Pacotes: java.sql e javax.sql (API suplementar para servidores)
+//
+public class Conexao
+{
+	public static void main(String[] args)
+	{
+	}
+}
---
+++ qacademicoJava/src/qacademico/inicio/Inicio.java
@@ -0,0 +1,9 @@
+package qacademico.inicio;
+
+public class Inicio
+{
+	public static void main(String[] args)
+	{
+		System.out.println("teste");
+	}
+}
