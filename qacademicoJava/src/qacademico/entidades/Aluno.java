299ecf0f776647eee5e54a965313490235324f96
pb7p <pb7p@MI00273033.petrobras.biz>
2019-08-10 00:18:56 -0300

    Setters&Getters em aluno.

--- qacademicoJava/src/qacademico/entidades/Aluno.java
+++ qacademicoJava/src/qacademico/entidades/Aluno.java
@@ -6,4 +6,44 @@ public class Aluno
 	private String nome;
 	private String sobrenome;
 	private String telefone;
+
+	public int getIdAluno()
+	{
+		return idAluno;
+	}
+
+	public String getNome()
+	{
+		return nome;
+	}
+
+	public String getSobrenome()
+	{
+		return sobrenome;
+	}
+
+	public String getTelefone()
+	{
+		return telefone;
+	}
+
+	public void setIdAluno(int idAluno)
+	{
+		this.idAluno = idAluno;
+	}
+
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+
+	public void setSobrenome(String sobrenome)
+	{
+		this.sobrenome = sobrenome;
+	}
+
+	public void setTelefone(String telefone)
+	{
+		this.telefone = telefone;
+	}
 }
