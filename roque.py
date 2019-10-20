69415dc8403c60391a0fd82f05b8068723c08ae0
AlexsandroMatias2017 <matiasalexsandro@gmail.com>
2019-10-20 13:37:18 -0300

    Criação de tabelas e deixando o programa mais interativo.

--- roque.py
+++ roque.py
@@ -8,101 +8,129 @@
 import mysql.connector
 
 conexao = mysql.connector.connect(
-    host="localhost", 
-    user="matias",  
-    passwd="matias",
-    database=""  
+	host="localhost", 
+	user="matias",  
+	passwd="matias",
+	database=""  
    
 )
 
 cursor = conexao.cursor()
 
 def executeSql(filename):
-    fd = open(filename, 'r')
-    sqlFile = fd.read()
-    fd.close()
-    sqlCommands = sqlFile.split(';')
+	fd = open(filename, 'r')
+	sqlFile = fd.read()
+	fd.close()
+	sqlCommands = sqlFile.split(';')
 
-    for comando in sqlCommands:
-        try:
-            if comando.strip() != '':
-                cursor.execute(comando)
-        except IOError as msg:
-            print("Comando falhou: ", msg)
+	for comando in sqlCommands:
+		try:
+			if comando.strip() != '':
+				cursor.execute(comando)
+		except IOError as msg:
+			print("Comando falhou: ", msg)
 
 
 
 def menuPrincipal():
-    print("O que deseja fazer: ")
-    print("1 - Consultar um Banco de Dados.")
-    print("2 - Remover um Banco de Dados.")
-    print("3 - Consultar uma tabela específica de um Banco de Dados.")
-    print("4 - Clonar um Banco de Dados.")
+	print("O que deseja fazer: ")
+	print("1 - Visualizar os Bancos de Dados existentes.")
+	print("2 - Remover um Banco de Dados.")
+	print("3 - Consultar uma tabela específica de um Banco de Dados.")
+	print("4 - Clonar um Banco de Dados.")
+	print("8 - Criar um Banco de Dados: ")
+	print("9 - Sair")
 
 
-    opcao = input("____________________________________________________________________________\n\n")
+	opcao = input("____________________________________________________________________________\n\n")
 
-    if opcao == '1':
-        mostrarBancos()
+	if opcao == '1':
+		mostrarBancos()
 
-    if opcao == '2':
-        removerBanco()
+	if opcao == '2':
+		removerBanco()
 
 
-    if opcao == '3':
-        mostrarTabelas()
+	if opcao == '3':
+		mostrarTabelas()
 
-    if opcao == '4':
-        clonarBanco()
+	if opcao == '4':
+		clonarBanco()
 
+	if opcao == '8':
+		criarManualDoBanco()
+
+
+	if opcao == '9':
+		sair()
 
 def iteracaoDosDados(sqlc):
    cursor.execute(sqlc)
    for x in cursor:
-      print(x)
+   	print(x)
+   print("____________________________________________________________________________")
 
-def criacaoBanco():
-    executeSql("somenteEstrutura.sql")
-    print("banco de dados criado com sucesso!")
-
-def insercaoDados():
-    executeSql("somenteDados.sql")
-    print("banco de dados inseridos com sucesso!")
 
 
 def mostrarBancos():
-    iteracaoDosDados("show databases")
+	iteracaoDosDados("show databases")
+	# menuPrincipal()
+
+def criarManualDoBanco():
+	bancoCriado = input("Digite o Banco de dados a ser criado: ")
+	consulta = "create database " + bancoCriado + ";"
+	iteracaoDosDados(consulta)
+
+def criacaoBanco():
+	executeSql("somenteEstrutura.sql")
+	print("banco de dados criado com sucesso!")
+	# menuPrincipal()
+
+def insercaoDados():
+	executeSql("somenteDados.sql")
+	print("banco de dados inseridos com sucesso!")
+	# menuPrincipal()
+
+
+
 
 def mostrarTabelas():
-    escolherBanco()
-    iteracaoDosDados("show tables")
-    tabela = input("Escolha uma das tabelas acima:")
-    consulta = "select * from  " + tabela
-    iteracaoDosDados(consulta)
+	escolherBanco()
+	iteracaoDosDados("show tables")
+	tabela = input("Escolha uma das tabelas acima:")
+	consulta = "select * from  " + tabela
+	iteracaoDosDados(consulta)
+	# menuPrincipal()
+
 
 def escolherBanco():
-    mostrarBancos()
-    bancoSelecionado = input("Digite o Banco de dados: ")
-    consulta = "use " + bancoSelecionado + ";"
-    iteracaoDosDados(consulta)
+	mostrarBancos()
+	bancoSelecionado = input("Digite o Banco de dados: ")
+	consulta = "use " + bancoSelecionado + ";"
+	iteracaoDosDados(consulta)
 
 def removerBanco():
-    mostrarBancos()
-    bancoSelecionado = input("Digite o Banco de dados a ser removido: ")
-    consulta = "drop database " + bancoSelecionado + ";"
-    iteracaoDosDados(consulta)
+	mostrarBancos()
+	bancoSelecionado = input("Digite o Banco de dados a ser removido: ")
+	consulta = "drop database " + bancoSelecionado + ";"
+	iteracaoDosDados(consulta)
 
 def clonarBanco():
-    mostrarBancos()
-    bancoSelecionado = input("Digite o Banco de dados a ser clonado: ")
-    
+	mostrarBancos()
+	bancoSelecionado = input("Digite o Banco de dados a ser clonado: ")
+	# menuPrincipal()
+	
 
+def sair():
+	exit()
 
+opcao = ''
 
-# menuPrincipal()
-criacaoBanco()
-insercaoDados()
-
+while opcao != 9:
+	menuPrincipal()
+# criacaoBanco()
+# insercaoDados()
+# 
 
 
 
