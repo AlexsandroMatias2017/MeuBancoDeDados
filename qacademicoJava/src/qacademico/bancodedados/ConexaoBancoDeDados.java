8a12969565413b5bb3e9891986d3d7058ecc6065
Alexsandro Matias de Almeida <AlexsandroMatias2017>
2019-12-14 16:33:54 -0300

    deleção da pasta

--- qacademicoJava/src/qacademico/bancodedados/ConexaoBancoDeDados.java
+++ qacademicoJava/src/qacademico/bancodedados/ConexaoBancoDeDados.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 //
 // Visão geral do JDBC
 // JDBC (Java Database Connectivity): API padr�o do Java para acesso a dados
-// P�ginas oficiais:
+// Páginas oficiais:
 // https://docs.oracle.com/javase/8/docs/technotes/guides/jdbc/
 // https://docs.oracle.com/javase/8/docs/api/java/sql/package-summary.html
 // Pacotes: java.sql e javax.sql (API suplementar para servidores)
